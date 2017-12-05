6f378bb17927ab4f58a1d6c7595f9e1fcf5f704c
macoskey <macoskey@umich.edu>
2017-12-05 15:45:23 -0500

    added support for dual class synth generation

--- Code/synthetic_data_generator.m
+++ Code/synthetic_data_generator.m
@@ -5,12 +5,12 @@
 % 
 
 
-%% Synthetic data synthesis
+%% Synthetic data synthesis (Support Class)
 % Initialize simulation parameters
 N = 90;                     % simulate a 3-month period
-M = 1024;                   % number of synthetic stocks
+M = 4096;                   % number of synthetic stocks
 support = rand();
-synth_data = zeros(N,M);    % matrix of valid synthetic data
+synth_data_C1 = zeros(N,M);    % matrix of valid synthetic data
 synth_cnt = 1;
 
 price_mu = 0;
@@ -58,27 +58,55 @@ while synth_cnt <= M
     % save synthetic stock if it meets the desired parameters
     if hit_cntr >=2
         % Make stock prices all positive valued
-        synth_data(:,synth_cnt) = price+abs(min(price));
+        synth_data_C1(:,synth_cnt) = price+abs(min(price));
         synth_cnt = synth_cnt + 1;
         fprintf('%.1d synthetic stocks generated\n',synth_cnt-1)
     end
 end
-
-%% Show montage of the synthetic data
+save(['synth_C1_',num2str(date()),'.mat'], 'synth_data_C1')
+%% Show montage of the synthetic support class data
 figure(1)
 recurrence_data = zeros(N,N,1,M);
-for i = 1:size(synth_data,2)
-    X = synth_data(:,i)*synth_data(:,i)';
+for i = 1:size(synth_data_C1,2)
+    X = synth_data_C1(:,i)*synth_data_C1(:,i)';
     recurrence_data(:,:,1,i) = X;
 end
-q = quantile(recurrence_data(:),256);
+q = quantile(recurrence_data(:),32);
 montage(recurrence_data,...
-    'Size',[32 32],'DisplayRange',[0 q(end)])
+    'Size',[sqrt(M) sqrt(M)],'DisplayRange',[0 q(end)])
 
+%% Synthetic Data Generator (NO support class)
 
+N = 90;                     % simulate a 3-month period
+M = 4096;                   % number of synthetic stocks
+synth_data_C2 = zeros(N,M);    % matrix of valid synthetic data
+price_mu = 0;
+price_sigma = 1;
 
+for m = 1:M  
+    price = zeros(1,N);         % single stock vector
+    price(1) = normrnd(price_mu,price_sigma);
 
+    for n = 2:N
+        price(n) = price(n-1) + normrnd(price_mu,price_sigma);
+    end
+ 
+    % Make stock prices all positive valued
+    synth_data_C2(:,m) = price+abs(min(price));
+    fprintf('%.1d synthetic stocks generated\n',m)
+end
+save(['synth_C2_',num2str(date()),'.mat'], 'synth_data_C2')
 
+%% Show montage of the synthetic non-support class data
+figure(2)
+recurrence_data = zeros(N,N,1,M);
+for i = 1:size(synth_data_C2,2)
+    X = synth_data_C2(:,i)*synth_data_C2(:,i)';
+    recurrence_data(:,:,1,i) = X;
+end
+q = quantile(recurrence_data(:),32);
+montage(recurrence_data,...
+    'Size',[sqrt(M) sqrt(M)],'DisplayRange',[0 q(end)])
 
 
 
